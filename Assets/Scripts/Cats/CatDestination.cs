68aba42110bfe431adbe1b26c93af610daaf97ed
Zera <guillegar17@gmail.com>
2023-08-19 17:31:40 -0300

    Gatos con sprites que cambian según su estado, con timer de paciencia y con maullidos. Soporte para variantes de gatos.

--- Assets/Scripts/Cats/CatDestination.cs
+++ Assets/Scripts/Cats/CatDestination.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatDestination : MonoBehaviour
-{
+public class CatDestination : MonoBehaviour {
+    public Cat currentCat;
     // Start is called before the first frame update
     void Start() {
         CatManager.AddDestination(this);
