8b258cb2c093f824c748b628f75b1518c705ff77
Rodrigo <rodri5202@hotmail.com>
2023-08-19 15:35:30 -0300

    Stuff:
    WIP Menu
    Creada Asset Font de Funnier.ttf para Textmeshpro
    ajustes al dragdrop, consume (dragdrop ahora chequea por el tag, si corresponde, el portador de consume emite sonido y se autodestruye)

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -3,27 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DragDrop : MonoBehaviour
-{
-    private AudioSource audioJungle;
+{   
+    public AudioClip SelectNoise;
+    public AudioSource audioJungle;
+    public Consume Consumer;
+    private Collider2D coll;
+    private bool onMouth;
 
     private void Start()
     {
-        audioJungle = gameObject.GetComponent<AudioSource>();
+        onMouth = false;
+        coll = gameObject.GetComponent<BoxCollider2D>();
+        audioJungle = GameObject.Find("Main Camera").GetComponent<AudioSource>();
     }
 
     private void OnMouseDown()
     {
-        audioJungle.Play();
+        audioJungle.PlayOneShot(SelectNoise, 0.7f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Consumidor")
+        {
+            onMouth = true;
+            Debug.Log("Food on mouth");
+            Consumer = collision.GetComponent<Consume>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Consumidor")
+        {
+            onMouth = false;
+            Debug.Log("Food left mouth");
+            Consumer = null;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (onMouth)
+        {
+            Consumer.StartCoroutine("Eat");
+            GameObject.Destroy(gameObject);
+        }
     }
 
     private void OnMouseDrag()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         transform.Translate(mousePos);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }    
 }
