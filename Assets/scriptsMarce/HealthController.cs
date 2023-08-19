ab796bb3324b523187a4170c065f1167bfa4ef5b
Merzbro00 <142639878+Merzbro00@users.noreply.github.com>
2023-08-19 13:22:05 -0300

    MarceHP
    Escena nueva (MarceHP) con barra de vida regenerable y botón pa testear

---
+++ Assets/scriptsMarce/HealthController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth = 100f;
+    public float healSpeed = 10f;
+    public Slider healthSlider;
+
+    public float damageAmount = 10f; // Amount of damage to deal
+
+    private void Start()
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
+    }
+
+    private void Update()
+    {
+        if (currentHealth < maxHealth)
+        {
+            currentHealth = Mathf.Clamp(currentHealth + healSpeed * Time.deltaTime, 0f, maxHealth);
+            healthSlider.value = currentHealth;
+        }
+    }
+
+    public void TakeDamage()
+    {
+        currentHealth = Mathf.Clamp(currentHealth - damageAmount, 0f, maxHealth);
+        healthSlider.value = currentHealth;
+    }
+}
